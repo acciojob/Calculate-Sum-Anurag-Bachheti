89675d9d293787aea2f095e41910e9758deead6f
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-08-27 12:35:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const num1 = document.getElementById("n1").value;
+const num2 = document.getElementById("n2").value;
+const btn = document.getElementById("sum_button");
+const sum = document.getElementById("sum");
+
+btn.addEventListener("click", function(){
+	sum.textContent = num1 + num2;
+})
