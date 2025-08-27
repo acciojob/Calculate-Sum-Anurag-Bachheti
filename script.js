cc31338921b4ca010c2d54ee6cbe400a5ecc5ace
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-08-27 13:06:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const num1 = document.getElementById("n1").value;
-const num2 = document.getElementById("n2").value;
-const btn = document.getElementById("sum_button");
+const btn = document.getElementById("sum_btn");
+const num1 = document.getElementById("n1");
+const num2 = document.getElementById("n2");
 const sum = document.getElementById("sum");
 
 btn.addEventListener("click", function(){
-	sum.textContent = num1 + num2;
+	const val1 = parseFloat(num1.value)
+	const val2 = parseFloat(num2.value)
+	sum.textContent = val1 + val2;
 })
